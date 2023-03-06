c7e0a7ab4d5ace2e55391c8d05f2dfda61086ddf
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-03-07 03:16:47 +0500

    react query

--- Adopt-Me/src/App.jsx
+++ Adopt-Me/src/App.jsx
@@ -1,19 +1,35 @@
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <h1>Adopt Me!</h1>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
