bc677e4ae0f691a7adbf266355a7c6772018bddc
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-03-22 16:04:50 +0500

    modal with portals

---
+++ Adopt-Me/src/ErrorBoundary.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStatefromError() {
+    return { hasError: true };
+  }
+componentDidCatch(error,info){
+console.log("Error boundary caught an error", error, info);
+}
+render(){
+
+
+    if(this.state.hasError){
+        return (
+            <h2> There was error in this listening . <Link to="/"> Click here to go back to home page</Link></h2>
+        )
+    }
+    return this.props.children
+}
+
+
+}
+
+export default ErrorBoundary
